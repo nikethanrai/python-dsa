92b6e33273256ea410441b9c6dd8a6dd027b301e
nikethan rai <58390625+nikethanrai@users.noreply.github.com>
2022-02-07 20:55:43 +0530

    add

---
+++ Sort/findAllRepeat.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findDuplicates(self, arr: List[int]) -> List[int]:
+        i = 0
+        miss = []
+        while i < len(arr):
+            correct = arr[i] - 1
+            if arr[i] != arr[correct]:
+                swap(arr, i, correct)
+            else:
+                i += 1
+        j = 0
+        while j < len(arr):
+            if arr[j] != j + 1:
+                miss.append(arr[j])
+            j += 1
+
+        return miss
+
+
+def swap(arr,i,correct):
+    temp=arr[i]
+    arr[i]=arr[correct]
+    arr[correct]=temp
